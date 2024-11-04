037865c55683feb0fb0270753a761257dac5afd0
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2024-11-04 21:09:08 +0100

    [Code] InputHandler class, Camera injector

---
+++ Assets/_JAM/Scripts/Input/InputHandler.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementContext
+{
+    public Vector2 MovementInput { get; private set; }
+    public void SetMovementInput(Vector2 newInput) => MovementInput = newInput;
+    
+    public bool IsJumpPressedInput { get; private set; }
+    public void SetJumpIsPressedInput(bool newInput) => IsJumpPressedInput = newInput;
+}
+
+public class MouseContext
+{
+    public Vector2 MouseDeltaInput { get; private set; }
+    public void SetMouseDeltaInput(Vector2 newInput) => MouseDeltaInput = newInput;
+}
+
+[CreateAssetMenu(menuName = "GameJamScriptable", fileName = "InputHandler")]
+public class InputHandler : ScriptableObject, GameMappings.IGameplayActions
+{
+    private GameMappings m_MappingsInstance = null;
+    public MouseContext InputMouseContext { get; private set; }
+    public MovementContext InputMovementContext { get; private set; }
+    
+    private void OnEnable()
+    {
+        if(m_MappingsInstance == null)
+            m_MappingsInstance = new();
+
+        InputMouseContext = new();
+        InputMovementContext = new();
+        
+        m_MappingsInstance.Enable();
+    }
+
+    private void OnDisable()
+    {
+        m_MappingsInstance.Disable();
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        if (context.phase != InputActionPhase.Performed)
+        {
+            return;
+        }
+
+        Debug.Log("MovementInput");
+    }
+
+    public void OnMouseMovement(InputAction.CallbackContext context)
+    {
+        if(context.phase != InputActionPhase.Performed)
+        {
+            return;
+        }
+        
+        Debug.Log("MouseInput");
+    }
+}
