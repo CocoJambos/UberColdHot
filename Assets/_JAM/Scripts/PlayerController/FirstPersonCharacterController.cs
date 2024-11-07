20bed057c7b4cb44d278d5ea45d4ea3dea5a68bf
Mateusz Budny <mateusz.budny97@gmail.com>
2024-11-07 02:49:24 +0100

    Sliding movement added. TweenController - local tween for object target added.

--- Assets/_JAM/Scripts/PlayerController/FirstPersonCharacterController.cs
+++ Assets/_JAM/Scripts/PlayerController/FirstPersonCharacterController.cs
@@ -1,5 +1,4 @@
 using ECM2;
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,9 +8,14 @@ public class FirstPersonCharacterController : Character
     [SerializeField] private CameraController m_CameraController;
     [SerializeField] private AnimationCurve m_AccelerationCurve;
 
+    [Header("Custom Movement Modes")]
+    [SerializeField] private SlidingMovement slidingMovement;
+
+    private bool CanSlide => IsWalking();
+
     private float m_BaseMaxAcceleration;
     private bool m_WasJumpTriggered;
-    
+
     protected override void Start()
     {
         base.Start();
@@ -41,18 +45,17 @@ public class FirstPersonCharacterController : Character
         {
             brakingDecelerationWalking += 0.1f;
         }
-        
+
         Vector2 movementInput = m_InputHandler.InputMovementContext.MovementInput;
         Vector3 movementDirection = new(movementInput.x, 0, movementInput.y);
         movementDirection = m_CameraController.RelativeToCamera(movementDirection);
         SetMovementDirection(movementDirection);
-        
+
         Vector2 mouseInput = m_InputHandler.InputMouseContext.MouseDeltaInput;
         m_CameraController.AddCameraInput(-mouseInput.y);
         AddYawInput(mouseInput.x);
 
         bool isJumpPressed = m_InputHandler.InputMovementContext.IsJumpPressedInput;
-
         if(isJumpPressed && CanJump())
         {
             if(!m_WasJumpTriggered)
@@ -65,11 +68,21 @@ public class FirstPersonCharacterController : Character
         {
             StopJumping();
         }
+
+        bool isSlidePressed = m_InputHandler.InputMovementContext.IsSlidePressedInput;
+        if(isSlidePressed && CanSlide && !IsSliding())
+        {
+            SetMovementMode(MovementMode.Custom, (int)ECustomMovementMode.Sliding);
+        }
+        else if(!isSlidePressed && IsSliding())
+        {
+            SetMovementMode(MovementMode.Walking);
+        }
     }
-    
-    
+
+
     protected override void OnBeforeSimulationUpdate(float deltaTime)
-    { 
+    {
         base.OnBeforeSimulationUpdate(deltaTime);
 
         if(IsWalking())
@@ -82,23 +95,23 @@ public class FirstPersonCharacterController : Character
             float normalizedSpeed = GetSpeed() / GetMaxSpeed();
             float accelerationFactor = m_AccelerationCurve.Evaluate(normalizedSpeed);
             maxAcceleration = m_BaseMaxAcceleration * accelerationFactor;
-            
+
             CheckAndTriggerBlockDisappearing(characterMovement.currentGround.collider);
         }
     }
 
-    private void CheckAndTriggerBlockDisappearing(Collider collider)
+    public void CheckAndTriggerBlockDisappearing(Collider collider)
     {
-        if(collider.CompareTag("BasicBlock") && collider.TryGetComponent(out BlockDisappearing blockDisappearing))
+        if(collider.CompareTag(Tags.BasicBlock.ToString()) && collider.TryGetComponent(out BlockDisappearing blockDisappearing))
         {
             blockDisappearing.TryToStartDisappearingOnPlayerTouch();
         }
     }
-    
+
     protected override void OnCollided(ref CollisionResult collisionResult)
     {
         base.OnCollided(ref collisionResult);
-        
+
         CheckAndTriggerBlockDisappearing(collisionResult.collider);
     }
 }
