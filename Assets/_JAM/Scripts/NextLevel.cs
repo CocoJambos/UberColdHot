fb6c848da7d1680a80a928724d9d413c32d4ea20
Mateusz Budny <mateusz.budny97@gmail.com>
2024-11-08 22:00:34 +0100

    FinishLine improvements. Main Scene level imporovements.

--- Assets/_JAM/Scripts/NextLevel.cs
+++ Assets/_JAM/Scripts/NextLevel.cs
@@ -1,27 +1,41 @@
-using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
     public float lodalLevelDelay;
-    
+    [SerializeField]
+    private bool allObjectivesCompleted = false;
+    [SerializeField]
+    private UnityEvent onBeforeNewLevelLoaded;
+
     private WaitForSeconds delayRoutine;
 
     private void Awake()
     {
-        delayRoutine = new WaitForSeconds(lodalLevelDelay);    
+        delayRoutine = new WaitForSeconds(lodalLevelDelay);
     }
 
-    public void LoadNextLevel()
+    public void SetAllObjectivesCompleted(bool areAllObjectivesCompleted)
     {
+        allObjectivesCompleted = areAllObjectivesCompleted;
+    }
+
+    public void LoadNextLevelIfAllObjectivesAreCompleted()
+    {
+        if(!allObjectivesCompleted)
+            return;
+
+        onBeforeNewLevelLoaded.Invoke();
         StartCoroutine(Delay());
     }
 
     private IEnumerator Delay()
     {
         yield return new WaitForSeconds(lodalLevelDelay);
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
