116a553cd897e408f2f69a7c958eaf7d9ec1d79d
LootDigger <ulysses@mail.com>
2024-11-08 21:35:28 +0100

    Added nextLevel delay 2 seconds

--- Assets/_JAM/Scripts/NextLevel.cs
+++ Assets/_JAM/Scripts/NextLevel.cs
@@ -1,10 +1,27 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
+    public float lodalLevelDelay;
+    
+    private WaitForSeconds delayRoutine;
+
+    private void Awake()
+    {
+        delayRoutine = new WaitForSeconds(lodalLevelDelay);    
+    }
+
     public void LoadNextLevel()
     {
+        StartCoroutine(Delay());
+    }
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(lodalLevelDelay);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
