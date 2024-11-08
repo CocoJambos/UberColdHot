98e10a1d95ac0dc8bb6a107fa780c994667ac2f7
LootDigger <ulysses@mail.com>
2024-11-08 20:59:15 +0100

    Cube disappearing sound removed

--- Assets/_JAM/Scripts/PhysicalButton.cs
+++ Assets/_JAM/Scripts/PhysicalButton.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PhysicalButton : MonoBehaviour
 {
+    [SerializeField] private UnityEvent OnPressed;
     public bool IsPressed { get; private set; } = false;
 
     public void Press()
     {
+        if (IsPressed) {return;}
         IsPressed = true;
+        Debug.Log(gameObject.name + " is pressed");
+        
+        if(OnPressed != null)
+            OnPressed.Invoke();
     }
 }
